5b2032557e43d38ca5568dc40be38bea2f74edb5
AgustinesJhonCarlo <152110217+AgustinesJhonCarlo@users.noreply.github.com>
2023-12-08 21:10:02 +0800

    add user gui

---
+++ UserView.java
@@ -0,0 +1,59 @@
+package offense.management.user;
+
+import javax.swing.*;
+import java.awt.*;
+
+class UserView{
+
+  static void addButton(JPanel panel, JButton button, int x, int y, int width, int height, int fontSize, String fontStyle, int color, int foreColor){
+    button.setBounds(x, y, width, height);
+    button.setFont(new Font(fontStyle, Font.BOLD, fontSize));
+    button.setBackground(new Color(color));
+    button.setForeground(new Color(foreColor));
+    button.setBorder(null);
+    button.setFocusable(false);
+
+    panel.add(button);
+  }
+  static void addLabel(JPanel panel, JLabel label, int x, int y, int width, int height, int fontSize, String fontStyle, int color){
+    label.setBounds(x, y, width, height);
+    label.setBackground(null);
+    label.setForeground(new Color(color));
+    label.setFont(new Font(fontStyle, Font.BOLD, fontSize));
+    label.setHorizontalAlignment(JLabel.CENTER);
+
+    panel.add(label);
+  }
+
+  static void addLabel(JPanel panel, JLabel label, int x, int y, int width, int height, int fontSize, String fontStyle){
+    label.setBounds(x, y, width, height);
+    label.setBackground(Color.white);
+    label.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
+    label.setHorizontalAlignment(JLabel.LEFT);
+
+    panel.add(label);
+  }
+
+  static void addTextArea(JPanel panel, JTextArea textField, int x, int y, int width, int height, String fontStyle, int fontSize){
+      textField.setBounds(x, y, width, height);
+      textField.setBackground(null);
+      textField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
+      textField.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
+    
+      panel.add(textField); 
+    }  
+
+    static void addPanel(JFrame frame, JPanel panel, int x, int y, int width, int height, int color){
+      panel.setBounds(x, y, width, height);
+      panel.setOpaque(true);
+      panel.setBackground(new Color(color));
+      panel.setLayout(null);  
+
+      frame.add(panel);
+    }
+    static void addImage(JPanel panel, ImageIcon img, int x, int y){
+      JLabel label = new JLabel(img);
+      label.setBounds(x, y, img.getIconWidth(), img.getIconHeight());
+      panel.add(label);
+    }
+}
