17e28a1d7fed24f06a43e68824f1d779c93f5039
Lloydjohn <146814824+Lloydjohn@users.noreply.github.com>
2023-12-08 19:36:20 +0800

    Update Register ui

--- Register/RegisterController.java
+++ Register/RegisterController.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class RegisterController{
   public static void main(String args[]){
     JFrame frame = new JFrame();
-    frame.setSize(835, 670);
+    frame.setSize(847, 670);
     frame.setLayout(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setResizable(false);
@@ -14,47 +14,37 @@ public class RegisterController{
     frame.getContentPane().setBackground(Color.black);
 
     ImageIcon bgImg = new ImageIcon("register.png");
-    JLabel imgHold = new JLabel(bgImg);
-    RegisterView.addImage(frame, imgHold, bgImg, 10, 0);
 
-    ImageIcon bgImg2 = new ImageIcon("woman.png");
-    JLabel imgHold2 = new JLabel(bgImg2);
-    RegisterView.addImage(frame, imgHold2, bgImg2, bgImg.getIconWidth(), 0);
-
-    JPanel RegisterPanel = new JPanel();
-    RegisterView.addPanel(frame, RegisterPanel, 10,152,359, 470, 0xCCCCCC);
-
-    JLabel firstnameLbl = new JLabel("First Name:");
-    RegisterView.addLabel(RegisterPanel, firstnameLbl, 40, 10, 120, 40, 20, "Roboto");
+    JPanel registerInputPanel = new JPanel();
+    RegisterView.addPanel(frame, registerInputPanel, 10, 140, 363, 370, 0xcccccc);
+    registerInputPanel.setOpaque(false);
 
     JTextField firstnameTxt = new JTextField();
-    RegisterView.addTextField(RegisterPanel, firstnameTxt, 40, 50, 270, 40, "Roboto", 20);
-
-    JLabel lastnameLbl = new JLabel("Last Name:");
-    RegisterView.addLabel(RegisterPanel, lastnameLbl, 40, 90, 120, 40, 20, "Roboto");
+    RegisterView.addTextField(registerInputPanel, firstnameTxt, 35, 50, 296, 40, "Roboto", 20);
 
     JTextField lastnameTxt = new JTextField();
-    RegisterView.addTextField(RegisterPanel, lastnameTxt, 40, 130, 270, 40, "Roboto", 20);
-
-    JLabel usernameLbl = new JLabel("Username:");
-    RegisterView.addLabel(RegisterPanel, usernameLbl, 40, 170, 120, 40, 20, "Roboto");
+    RegisterView.addTextField(registerInputPanel, lastnameTxt, 35, 130, 296, 40, "Roboto", 20);
 
     JTextField usernameTxt = new JTextField();
-    RegisterView.addTextField(RegisterPanel, usernameTxt, 40, 210, 270, 40, "Roboto", 20);
-
-    JLabel passwordLbl = new JLabel("Password:");
-    RegisterView.addLabel(RegisterPanel, passwordLbl, 40, 250, 120, 40, 20, "Roboto");
+    RegisterView.addTextField(registerInputPanel, usernameTxt, 35, 210, 296, 40, "Roboto", 20);
 
     JPasswordField passwordTxt = new JPasswordField();
-    RegisterView.addJPasswordField(RegisterPanel, passwordTxt, 40, 290, 270, 40, "Roboto", 20);
+    RegisterView.addJPasswordField(registerInputPanel, passwordTxt, 35, 290, 296, 40, "Roboto", 20);
 
-    JButton cancelButton = new JButton("CANCEL");
-    RegisterView.addButton(RegisterPanel, cancelButton, 15, 420,160, 30, "Roboto", 22, 0xC5C5C5, 0x626262  );
+    JPanel buttonPanel = new JPanel();
+    RegisterView.addPanel(frame, buttonPanel, 10, 515, 363, 100, 0x626262);
+    buttonPanel.setOpaque(false);
 
-    JButton registerButton = new JButton("REGISTER");
-    RegisterView.addButton(RegisterPanel, registerButton, 185, 420,160, 30, "Roboto", 22, 0xC5C5C5, 0x626262  );
+    JLabel cancelButton = new JLabel("CANCEL");
+    RegisterView.addLabel(buttonPanel, cancelButton, 18, 43,160, 30, 20, "Roboto", 0xcccccc);
+
+    JLabel registerButton = new JLabel("REGISTER");
+    RegisterView.addLabel(buttonPanel, registerButton, 185, 43,160, 30, 20, "Roboto", 0xcccccc);
 
 
+    JPanel registerPanel = new JPanel();
+    RegisterView.addPanel(frame, registerPanel, 10,0, bgImg.getIconWidth(), bgImg.getIconHeight(), 0xCCCCCC);
+    RegisterView.addImage(registerPanel, bgImg, 0, 0);
 
     frame.setVisible(true);
   }
--- Register/RegistervView.java
+++ Register/RegistervView.java
@@ -5,31 +5,12 @@ import java.awt.*;
 
 class RegisterView{
 
-  static void addButton(JPanel panel, JButton button, int x, int y, int width, int height, String fontStyle, int fontSize, int color, int bgColor){
-      button.setBounds(x, y, width, height);
-      button.setFont(new Font(fontStyle, Font.BOLD, fontSize));
-      button.setBackground(new Color(bgColor));
-      button.setBorder(null);
-      button.setForeground(new Color(color));
-      button.setFocusable(false);
-      button.setVisible(true);
-     
-      panel.add(button);
-    }
-  
-  static void addLabel(JPanel panel, JLabel label, int x, int y, int width, int height, int fontSize, String fontStyle){
-    label.setBounds(x, y, width, height);
-    label.setBackground(null);
-    label.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
-
-    panel.add(label);
-  }
-
   static void addLabel(JPanel panel, JLabel label, int x, int y, int width, int height, int fontSize, String fontStyle, int color){
     label.setBounds(x, y, width, height);
     label.setBackground(null);
     label.setForeground(new Color(color));
-    label.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
+    label.setFont(new Font(fontStyle, Font.BOLD, fontSize));
+    label.setHorizontalAlignment(JLabel.CENTER);
 
     panel.add(label);
   }
@@ -37,22 +18,22 @@ class RegisterView{
   static void addTextField(JPanel panel, JTextField textField, int x, int y, int width, int height, String fontStyle, int fontSize){
       textField.setBounds(x, y, width, height);
       textField.setBackground(null);
-      textField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.black));
+      textField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
       textField.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
     
       panel.add(textField); 
     }  
 
-    static void addJPasswordField(JPanel panel, JPasswordField passField, int x, int y, int width, int height, String fontStyle, int fontSize){
+  static void addJPasswordField(JPanel panel, JPasswordField passField, int x, int y, int width, int height, String fontStyle, int fontSize){
       passField.setBounds(x, y, width, height);
       passField.setBackground(null);
-      passField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.black));
+      passField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
       passField.setFont(new Font(fontStyle, Font.PLAIN, fontSize));
     
       panel.add(passField); 
     } 
 
-    static void addPanel(JFrame frame, JPanel panel, int x, int y, int width, int height, int color){
+  static void addPanel(JFrame frame, JPanel panel, int x, int y, int width, int height, int color){
       panel.setBounds(x, y, width, height);
       panel.setOpaque(true);
       panel.setBackground(new Color(color));
@@ -60,8 +41,9 @@ class RegisterView{
 
       frame.add(panel);
     }
-    static void addImage(JFrame frame, JLabel label, ImageIcon img, int x, int y){
+  static void addImage(JPanel panel, ImageIcon img, int x, int y){
+      JLabel label = new JLabel(img);
       label.setBounds(x, y, img.getIconWidth(), img.getIconHeight());
-      frame.add(label);
+      panel.add(label);
     }
 }
